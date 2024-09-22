7ab9586198be9ba3ae667bdace654786a3d66314
TheRoookie <parthbharvadiya@gmail.com>
2024-09-22 14:55:34 +0530

    added run.bat

--- src/Component/nav_components/How_it_works.js
+++ src/Component/nav_components/How_it_works.js
@@ -2,10 +2,10 @@ import React from 'react'
 function How_it_works() {
   return (
     <>
-  
-    <center><h1>Earn money by solving problems</h1></center>
-</>
-  ) 
+
+      <center><h1>Earn money by completing tasks</h1></center>
+    </>
+  )
 }
 
 export default How_it_works
